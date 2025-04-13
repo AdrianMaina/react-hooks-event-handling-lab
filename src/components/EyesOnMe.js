b7ddc2bddeb595b27f24cb72ee5581f589ed25b0
Haron M Maina <haronmmaina@Harons-MacBook-Air.local>
2025-04-13 15:56:10 +0300

    Adds changes to the repo

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function FocusHandler() {
+      console.log('Good!');
+    }
+  
+    function HandleBlur() {
+      console.log('Hey! Eyes on me!');
+    }
+
+       
+        return(
+
+      <div>
+        <button onFocus={FocusHandler} onBlur={HandleBlur}>Eyes on me</button>
+        </div>
+
+        
+    )
+}
+
+export default EyesOnMe;
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    
+        function ChangeHandler(){
+            console.log('Entering password...')
+        }
+
+    
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={ChangeHandler}></input>
+        </div>
     )
 }
 
